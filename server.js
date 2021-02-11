49e9f677dc089b71ba2d8a5e3e46887906354693
joshuavaslie2021 <joshuavaslie2021@u.northwestern.edu>
2021-02-11 16:39:02 -0600

    revised routes in serverjs

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -30,8 +30,24 @@ mongoose.connect(
 // });
 
 // routes
-app.use(require("./routes/api.js"));
+require("./routes/api-routes.js")(app);
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
+
+app.listen(PORT, () => {
+  
+  console.log(`App running on port ${PORT}!`);
+});
 // Start the server
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
